8a86ad2987f3187a5d38aa3ae35c93e15362b3c0
Olivia Tran <olivia.tran@walmart.com>
2022-12-28 17:00:25 -0800

    add generics to merge function

--- dist/index.js
+++ dist/index.js
@@ -1,13 +1,46 @@
-"use strict";
-const btn = document.getElementById("btn"); //Type assertion
-const input = document.getElementById("todoinput");
-const form = document.querySelector("form");
-const list = document.getElementById("todolist");
-const todos = readTodos();
+var __extends = (this && this.__extends) || (function () {
+    var extendStatics = function (d, b) {
+        extendStatics = Object.setPrototypeOf ||
+            ({ __proto__: [] } instanceof Array && function (d, b) { d.__proto__ = b; }) ||
+            function (d, b) { for (var p in b) if (Object.prototype.hasOwnProperty.call(b, p)) d[p] = b[p]; };
+        return extendStatics(d, b);
+    };
+    return function (d, b) {
+        if (typeof b !== "function" && b !== null)
+            throw new TypeError("Class extends value " + String(b) + " is not a constructor or null");
+        extendStatics(d, b);
+        function __() { this.constructor = d; }
+        d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
+    };
+})();
+var __assign = (this && this.__assign) || function () {
+    __assign = Object.assign || function(t) {
+        for (var s, i = 1, n = arguments.length; i < n; i++) {
+            s = arguments[i];
+            for (var p in s) if (Object.prototype.hasOwnProperty.call(s, p))
+                t[p] = s[p];
+        }
+        return t;
+    };
+    return __assign.apply(this, arguments);
+};
+function merge(object1, object2) {
+    return __assign(__assign({}, object1), object2);
+}
+console.log(merge({ name: "olivia" }, { languages: ["python", "javascript"] }));
+var inputElement = document.querySelector("#username");
+console.dir(inputElement);
+inputElement.value = "Hello";
+console.log(inputElement === null || inputElement === void 0 ? void 0 : inputElement.value);
+var btn = document.getElementById("btn"); //Type assertion
+var input = document.getElementById("todoinput");
+var form = document.querySelector("form");
+var list = document.getElementById("todolist");
+var todos = readTodos();
 todos.forEach(createTodo);
 // Load todos from local storage
 function readTodos() {
-    const todosJSON = localStorage.getItem("todos");
+    var todosJSON = localStorage.getItem("todos");
     if (todosJSON === null)
         return [];
     return JSON.parse(todosJSON);
@@ -18,9 +51,9 @@ function saveTodos() {
 }
 function handleSubmit(e) {
     e.preventDefault();
-    const newTodo = {
+    var newTodo = {
         text: input.value,
-        completed: false,
+        completed: false
     };
     createTodo(newTodo);
     todos.push(newTodo);
@@ -28,8 +61,8 @@ function handleSubmit(e) {
     input.value = "";
 }
 function createTodo(todo) {
-    const newLI = document.createElement("li");
-    const checkbox = document.createElement("input");
+    var newLI = document.createElement("li");
+    var checkbox = document.createElement("input");
     checkbox.type = "checkbox";
     checkbox.checked = todo.completed;
     checkbox.addEventListener("change", function () {
@@ -49,3 +82,105 @@ form.addEventListener("submit", handleSubmit);
 //   e.preventDefault();
 //   console.log("submitted");
 // });
+var Player = /** @class */ (function () {
+    function Player(first, last, _score) {
+        this.first = first;
+        this.last = last;
+        this._score = _score;
+    }
+    Player.prototype.secretMethod = function () {
+        console.log("SECRET METHOD!!");
+    };
+    Object.defineProperty(Player.prototype, "fullName", {
+        get: function () {
+            return "".concat(this.first, " ").concat(this.last);
+        },
+        enumerable: false,
+        configurable: true
+    });
+    Object.defineProperty(Player.prototype, "score", {
+        get: function () {
+            return this._score;
+        },
+        set: function (newScore) {
+            if (newScore < 0) {
+                throw new Error("Score cannot be negative!");
+            }
+            this._score = newScore;
+        },
+        enumerable: false,
+        configurable: true
+    });
+    return Player;
+}());
+var SuperPlayer = /** @class */ (function (_super) {
+    __extends(SuperPlayer, _super);
+    function SuperPlayer() {
+        var _this = _super !== null && _super.apply(this, arguments) || this;
+        _this.isAdmin = true;
+        return _this;
+    }
+    SuperPlayer.prototype.maxScore = function () {
+        this._score = 99999999;
+    };
+    return SuperPlayer;
+}(Player));
+var elton = new Player("Elton", "Steele", 100);
+elton.fullName;
+var Bike = /** @class */ (function () {
+    function Bike(color) {
+        this.color = color;
+    }
+    return Bike;
+}());
+var Jacket = /** @class */ (function () {
+    function Jacket(brand, color) {
+        this.brand = brand;
+        this.color = color;
+    }
+    Jacket.prototype.print = function () {
+        console.log("".concat(this.color, " ").concat(this.brand, " jacket"));
+    };
+    return Jacket;
+}());
+var bike1 = new Bike("red");
+var jacket1 = new Jacket("Prada", "black");
+var Employee = /** @class */ (function () {
+    function Employee(first, last) {
+        this.first = first;
+        this.last = last;
+    }
+    Employee.prototype.greet = function () {
+        console.log("HELLO!");
+    };
+    return Employee;
+}());
+var FullTimeEmployee = /** @class */ (function (_super) {
+    __extends(FullTimeEmployee, _super);
+    function FullTimeEmployee(first, last, salary) {
+        var _this = _super.call(this, first, last) || this;
+        _this.salary = salary;
+        return _this;
+    }
+    FullTimeEmployee.prototype.getPay = function () {
+        return this.salary;
+    };
+    return FullTimeEmployee;
+}(Employee));
+var PartTimeEmployee = /** @class */ (function (_super) {
+    __extends(PartTimeEmployee, _super);
+    function PartTimeEmployee(first, last, hourlyRate, hoursWorked) {
+        var _this = _super.call(this, first, last) || this;
+        _this.hourlyRate = hourlyRate;
+        _this.hoursWorked = hoursWorked;
+        return _this;
+    }
+    PartTimeEmployee.prototype.getPay = function () {
+        return this.hourlyRate * this.hoursWorked;
+    };
+    return PartTimeEmployee;
+}(Employee));
+var betty = new FullTimeEmployee("Betty", "White", 95000);
+console.log(betty.getPay());
+var bill = new PartTimeEmployee("Bill", "Billerson", 24, 1100);
+console.log(bill.getPay());
--- index.ts
+++ index.ts
@@ -1,3 +1,15 @@
+function merge(object1: { name: string }, object2: { languages: string[] }) {
+  return {
+    ...object1,
+    ...object2,
+  };
+}
+console.log(merge({ name: "olivia" }, { languages: ["python", "javascript"] }));
+
+const inputElement = document.querySelector<HTMLInputElement>("#username")!;
+console.dir(inputElement);
+inputElement!.value = "Hello";
+console.log(inputElement?.value);
 interface Todo {
   text: string;
   completed: boolean;
@@ -62,3 +74,103 @@ form.addEventListener("submit", handleSubmit);
 //   e.preventDefault();
 //   console.log("submitted");
 // });
+
+class Player {
+  constructor(
+    public first: string,
+    public last: string,
+    protected _score: number
+  ) {}
+
+  private secretMethod(): void {
+    console.log("SECRET METHOD!!");
+  }
+
+  get fullName(): string {
+    return `${this.first} ${this.last}`;
+  }
+
+  get score(): number {
+    return this._score;
+  }
+
+  set score(newScore: number) {
+    if (newScore < 0) {
+      throw new Error("Score cannot be negative!");
+    }
+    this._score = newScore;
+  }
+}
+
+class SuperPlayer extends Player {
+  public isAdmin: boolean = true;
+  maxScore() {
+    this._score = 99999999;
+  }
+}
+
+const elton = new Player("Elton", "Steele", 100);
+elton.fullName;
+// elton.score = "23";
+
+// Classes With Interfaces!
+interface Colorful {
+  color: string;
+}
+
+interface Printable {
+  print(): void;
+}
+
+class Bike implements Colorful {
+  constructor(public color: string) {}
+}
+
+class Jacket implements Colorful, Printable {
+  constructor(public brand: string, public color: string) {}
+
+  print() {
+    console.log(`${this.color} ${this.brand} jacket`);
+  }
+}
+
+const bike1 = new Bike("red");
+const jacket1 = new Jacket("Prada", "black");
+
+abstract class Employee {
+  constructor(public first: string, public last: string) {}
+  abstract getPay(): number;
+  greet() {
+    console.log("HELLO!");
+  }
+}
+
+class FullTimeEmployee extends Employee {
+  constructor(first: string, last: string, private salary: number) {
+    super(first, last);
+  }
+  getPay(): number {
+    return this.salary;
+  }
+}
+
+class PartTimeEmployee extends Employee {
+  constructor(
+    first: string,
+    last: string,
+    private hourlyRate: number,
+    private hoursWorked: number
+  ) {
+    super(first, last);
+  }
+  getPay(): number {
+    return this.hourlyRate * this.hoursWorked;
+  }
+}
+
+const betty = new FullTimeEmployee("Betty", "White", 95000);
+console.log(betty.getPay());
+
+const bill = new PartTimeEmployee("Bill", "Billerson", 24, 1100);
+
+console.log(bill.getPay());
